dfed8a4779677fb48b36f94cd6f1cef42db89324
Andrew Izatt <izatt.andrew@gmail.com>
2026-04-15 14:36:34 -0600

    Begin squirrel census

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pandas
-data = pandas.read_csv("./weather_data.csv")
-temp_list = data["temp"].to_list()
-avg_temp = sum(temp_list) / len(temp_list)
-print(len(temp_list))
-print(temp_list)
-print(avg_temp)
+# data = pandas.read_csv("./weather_data.csv")
+# temp_list = data["temp"].to_list()
+# avg_temp = sum(temp_list) / len(temp_list)
+# print(len(temp_list))
+# print(temp_list)
+# print(avg_temp)
+#
+# average = data["temp"].mean()
+# print(average)
+#
+# max_temp = data["temp"].max()
+# print(max_temp)
+#
+# print(data[data.temp == data.temp.max()])
 
-average = data["temp"].mean()
-print(average)
-
-max_temp = data["temp"].max()
-print(max_temp)
-
-print(data[data.temp == data.temp.max()])
+# ************ Squirrel Census **************
